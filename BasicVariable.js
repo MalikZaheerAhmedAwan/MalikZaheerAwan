8f03ff84bfe9fe86935bfbcd269175d65375472a
Malik Zaheer Ahmed Awan <malikzahawan@gmail.com>
2024-03-23 12:58:07 +0500

    First Commit

---
+++ BasicVariable.js
@@ -0,0 +1,7 @@
+var Name = "Malik Zaheer Ahmed Awan";
+var var1 = 7;
+var var2 = 5;
+console.log("My Name Is" + Name);
+console.log("First Value Is : " + var1);
+console.log("Second Value Is : " + var2);
+console.log("Sum of the Values Is : " + var1 + var2);
---
+++ BasicVariable.ts
@@ -0,0 +1,8 @@
+let Name="Malik Zaheer Ahmed Awan"
+
+let var1=7
+let var2=5
+console.log("My Name Is"+Name)
+console.log("First Value Is : "+var1)
+console.log("Second Value Is : "+var2)
+console.log("Sum of the Values Is : "+var1+var2)
